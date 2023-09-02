6d696c453e74b0c6e0bbbf714ff672a9fd039735
Onkar Marbhal <marbhal37@gmail.com>
2023-09-03 01:20:07 +0530

    feat (pattern) - singleton pattern added.

---
+++ patterns/src/main/java/singleton/ThreadSafeSingleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+public class ThreadSafeSingleton {
+
+    private static ThreadSafeSingleton INSTANCE = null;
+
+    private ThreadSafeSingleton(){}
+
+    public static ThreadSafeSingleton getInstance() {
+
+        if (INSTANCE == null){
+            synchronized (ThreadSafeSingleton.class) {
+
+                if(INSTANCE == null){
+                    INSTANCE = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
