a8b52cac52be1a0059c5dee1ea63aedd92cec9cc
Onkar Marbhal <marbhal37@gmail.com>
2023-09-24 16:38:49 +0530

    feat: implementation of Singleton with holder class.

---
+++ patterns/src/main/java/singleton/SingletonWithHolderClass.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class SingletonWithHolderClass {
+
+    private SingletonWithHolderClass(){}
+
+    private static final class InstanceHolder {
+        static final SingletonWithHolderClass INSTANCE = new SingletonWithHolderClass();
+    }
+
+    public static SingletonWithHolderClass getInstance() {
+
+        return InstanceHolder.INSTANCE;
+    }
+}
