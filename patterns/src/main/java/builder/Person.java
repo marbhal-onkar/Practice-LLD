0f23e081b4b87b3fb534aad6f90185291ab56152
Onkar Marbhal <marbhal37@gmail.com>
2023-10-05 00:14:36 +0530

    fix: Builder with annotation.

--- patterns/src/main/java/builder/Person.java
+++ patterns/src/main/java/builder/Person.java
@@ -1,64 +1,16 @@
 package builder;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * Normal class with getter and constructor
  */
 @Getter
 @ToString
+@Builder
 public class Person {
     private String firstName;
     private String lastName;
     private String description;
     private int age;
-
-    private Person() {
-    }
-
-    private Person(Person person) {
-        this.firstName = person.firstName;
-        this.lastName = person.lastName;
-        this.description = person.description;
-        this.age = person.age;
-    }
-
-    public static Builder builder(){
-        return new Builder();
-    }
-
-    public static class Builder {
-        private final Person person;
-
-        public Builder() {
-            person = new Person();
-        }
-
-        public Builder firstName(String firstName) {
-            person.firstName = firstName;
-            return this;
-        }
-
-        public Builder lastName(String lastName) {
-            person.lastName = lastName;
-            return this;
-        }
-
-        public Builder description(String description) {
-            person.description = description;
-            return this;
-        }
-
-        public Builder age(int age) {
-            person.age = age;
-            return this;
-        }
-
-        public Person build() {
-            return new Person(person);
-        }
-    }
 }
