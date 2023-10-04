0c4b9f2a5420b38d69765d802a6ae64a9f7ce076
Onkar Marbhal <marbhal37@gmail.com>
2023-10-04 18:42:14 +0530

    feat: simple demo classes for Builder added.

---
+++ patterns/src/main/java/builder/Client.java
@@ -0,0 +1,9 @@
+package builder;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Person person = new Person("Onkar", "Marbhal", "Architect", 26);
+        System.out.println(person.toString());
+    }
+}
---
+++ patterns/src/main/java/builder/Person.java
@@ -0,0 +1,19 @@
+package builder;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *  Normal class with getter and constructor
+ */
+@Getter
+@AllArgsConstructor
+@ToString
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final String description;
+    private final int age;
+}
